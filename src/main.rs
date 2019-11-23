481145cc0bfccc20105050113c782ebb0bfa6258
Masterchef365 <duncan.freeman1@gmail.com>
2019-11-22 22:23:17 -0800

    Untested, compiles though...

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
 mod pos58;
+use dither::color::*;
+use dither::ditherer::*;
 use escposify::printer::Printer;
-use failure::Fail;
 use failure::{format_err, Fallible};
-use image::{DynamicImage, ImageBuffer};
+use image::{imageops::resize, DynamicImage, FilterType, GenericImageView, RgbImage};
 use pos58::POS58USB;
-use std::fs::File;
-use std::io::BufRead;
-
-fn print_usage_and_exit() -> ! {
-    eprintln!("Args: <image path> <print width (ft)> Option<start row>");
-    std::process::exit(-1);
-}
-
-fn stdin_char() -> Fallible<char> {
-    let mut string = String::new();
-    std::io::stdin().read_line(&mut string)?;
-    string
-        .chars()
-        .nth(0)
-        .ok_or(format_err!("Expected input character"))
-        .into()
-}
 
 const IN_PER_FT: f32 = 12.0;
 const MM_PER_IN: f32 = 25.4;
+const PX_PER_FT: f32 = pos58::DOTS_PER_MM as f32 * MM_PER_IN * IN_PER_FT;
+const PRINTABLE_WIDTH_PX: u32 = pos58::DOTS_PER_MM * pos58::PRINTABLE_WIDTH_MM;
+const PAPER_WIDTH_PX: u32 = pos58::DOTS_PER_MM * pos58::PAPER_WIDTH_MM;
+
+fn print_usage_and_exit() -> ! {
+    eprintln!("Args: <image path> <actual width>");
+    std::process::exit(-1);
+}
 
 fn main() -> Fallible<()> {
-    // Parse args
     let mut args = std::env::args().skip(1);
 
     let image_file_path = match args.next() {
@@ -34,65 +25,89 @@ fn main() -> Fallible<()> {
         None => print_usage_and_exit(),
     };
 
-    let physical_width = match args.next() {
-        Some(w) => w,
+    let width_ft: f32 = match args.next() {
+        Some(p) => p.parse()?,
         None => print_usage_and_exit(),
     };
 
-    let start_row = args.next();
-
-    let physical_width: f32 = physical_width.parse()?;
-
     let image: DynamicImage = image::open(image_file_path)?;
-    let image = image.to_luma(); // Convert to grayscale
+    //let image = image.to_luma();
+    let image = image.to_rgb();
 
-    // Prompt if the height is ok
     let (image_width, image_height) = image.dimensions();
-    println!("Image dimensions: {}x{}", image_width, image_height);
-    let physical_height = (physical_width * image_height as f32) / image_width as f32;
-    println!(
-        "Image height is calculated to be {} ft. Is this acceptable? [Y/n]",
-        physical_height
-    );
-    /*if stdin_char()? == 'n' {
-        return Ok(());
-    } */
+    let virtual_width = (PX_PER_FT * width_ft as f32) as u32;
+    let virtual_height = (virtual_width * image_height as u32) / image_width as u32;
 
-    // The rest of the calculations are in pixels
-    let virtual_width = (physical_width * IN_PER_FT * MM_PER_IN) as u32;
-    for y_v in (0..virtual_height).step_by(pos58::PAPER_WIDTH_MM as usize) {
+    println!("Input dimensions: {}x{}", image_width, image_height);
+    println!("Pixel dimensions: {}x{}", virtual_width, virtual_height);
+
+    let mut usb_context = libusb::Context::new().expect("Failed to create LibUSB context.");
+
+    let mut device = POS58USB::new(&mut usb_context, std::time::Duration::from_secs(90))
+        .expect("Failed to connect to printer");
+
+    let mut printer = Printer::new(&mut device, None, None);
+
+    for row_beginning in (0..=virtual_height).step_by(PAPER_WIDTH_PX as usize) {
+        println!("{}", row_beginning);
+        let image_begin = image_height * row_beginning / virtual_height;
+        let image_crop_height = image_height * PRINTABLE_WIDTH_PX / virtual_height;
+
+        // Stretch the last row if it doesn't quite fit.
+        let image_view = if image_begin + image_crop_height <= image_height {
+            image.view(0, image_begin, image_width, image_crop_height)
+        } else {
+            println!("Stretched last.");
+            image.view(
+                0,
+                image_begin,
+                image_width,
+                image_height % image_crop_height,
+            )
+        };
+
+        let image_view = image_view.to_image();
+
+        let image_upscaled = resize(
+            &image_view,
+            virtual_width,
+            PRINTABLE_WIDTH_PX,
+            FilterType::Nearest,
+        );
+
+        println!("{:?}", image_view.dimensions());
+        println!("{:?}", image_upscaled.dimensions());
+
+        // Convert the image to a ditherable format
+        let ditherable_image = dither::prelude::Img::<RGB<u8>>::new(
+            image_upscaled.pixels().map(|p| RGB::from(p.data)),
+            image_upscaled.width(),
+        )
+        .ok_or(format_err!("Failed to convert to ditherable image"))?
+        .convert_with(|rgb| rgb.convert_with(f64::from));
+
+        // Dither the image using the SIERRA 3 algorithm
+        let dithering_palette = [RGB::from((255, 255, 255)), RGB::from((0, 0, 0))];
+        let dithered = SIERRA_3
+            .dither(ditherable_image, palette::quantize(&dithering_palette))
+            .convert_with(|rgb| rgb.convert_with(dither::prelude::clamp_f64_to_u8));
+
+        // Switch the image back to a consumable format for the printer
+        let converted_dither =
+            RgbImage::from_vec(dithered.width(), dithered.height(), dithered.raw_buf())
+                .ok_or(format_err!("Failed to convert dithered image to buffer"))?;
+        converted_dither.save(format!("{}.jpg", image_begin))?;
+
+        let printer_image = escposify::img::Image::from(DynamicImage::ImageRgb8(converted_dither));
+
+        // Print the image
+        printer
+            .align("lt")
+            .map_err(|_| format_err!("Failed to write to printer"))?;
+        printer
+            .bit_image(&printer_image, None)
+            .map_err(|_| format_err!("Failed to write to printer"))?;
     }
-
 
     Ok(())
 }
-
-/*
-let mut usb_context = libusb::Context::new().expect("Failed to create LibUSB context.");
-
-let mut device = POS58USB::new(&mut usb_context, std::time::Duration::from_secs(90))
-    .expect("Failed to connect to printer");
-
-let mut printer = Printer::new(&mut device, None, None);
-*/
-
-/*
- * Interactive: display the expected width and height...
- */
-
-/*
-let mut bitch = vec![0u8; (DOTS_PER_WIDTH * DOTS_PER_WIDTH) as usize];
-for (idx, pxl) in bitch.iter_mut().enumerate() {
-    if (idx & 1) == 0 {
-        *pxl = 255;
-    }
-}
-
-let converted = ImageBuffer::from_raw(DOTS_PER_WIDTH, DOTS_PER_WIDTH, bitch)
-    .ok_or("Failed to convert dithered image to buffer")?;
-let printer_image = escposify::img::Image::from(DynamicImage::ImageLuma8(converted));
-
-for _ in 0..2 {
-    printer.bit_image(&printer_image, None)?;
-}
-*/
